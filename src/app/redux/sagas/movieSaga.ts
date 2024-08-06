8aef1429b939f3c6f298bd1f371daa4b60e90b66
rizkifdh <rizki.fdz@gmail.com>
2024-08-06 13:01:25 +0700

    fix api url

--- src/app/redux/sagas/movieSaga.ts
+++ src/app/redux/sagas/movieSaga.ts
@@ -4,10 +4,11 @@ import { FetchMovieDetailAction, SearchMoviesAction } from '../types';
 import { AxiosResponse } from 'axios';
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 function* fetchMovies() {
   try {
-    const response: AxiosResponse = yield call(axios.get, `http://www.omdbapi.com/?apikey=${API_KEY}&s=avengers`);
+    const response: AxiosResponse = yield call(axios.get, `${API_URL}/?apikey=${API_KEY}&s=avengers`);
     yield put({ type: 'FETCH_MOVIES_SUCCESS', payload: response.data.Search });
   } catch (error: any) {
     yield put({ type: 'FETCH_FAILED', payload: error.message });
@@ -17,7 +18,7 @@ function* fetchMovies() {
 function* fetchPopularMovies(action: { type: string; payload: { page: number } }) {
   try {
     const { page } = action.payload;
-    const response: AxiosResponse = yield call(axios.get, `http://www.omdbapi.com/?apikey=${API_KEY}&s=movie&y=2024&page=${page}`);
+    const response: AxiosResponse = yield call(axios.get, `${API_URL}/?apikey=${API_KEY}&s=movie&y=2024&page=${page}`);
     yield put({ type: 'FETCH_POPULAR_MOVIES_SUCCESS', payload: response.data.Search });
   } catch (error: any) {
     yield put({ type: 'FETCH_FAILED', payload: error.message });
@@ -26,7 +27,7 @@ function* fetchPopularMovies(action: { type: string; payload: { page: number } }
 
 function* fetchMovieDetail(action: FetchMovieDetailAction) {
   try {
-    const response:AxiosResponse = yield call(axios.get, `http://www.omdbapi.com/?apikey=${API_KEY}&i=${action.payload}`);
+    const response:AxiosResponse = yield call(axios.get, `${API_URL}/?apikey=${API_KEY}&i=${action.payload}`);
     yield put({ type: 'FETCH_MOVIE_DETAIL_SUCCESS', payload: response.data });
   } catch (error:any) {
     yield put({ type: 'FETCH_FAILED', payload: error.message });
@@ -35,7 +36,7 @@ function* fetchMovieDetail(action: FetchMovieDetailAction) {
 
 function* searchMovies(action: SearchMoviesAction) {
   try {
-    const response:AxiosResponse = yield call(axios.get, `http://www.omdbapi.com/?apikey=${API_KEY}&s=${action.payload}`);
+    const response:AxiosResponse = yield call(axios.get, `${API_URL}/?apikey=${API_KEY}&s=${action.payload}`);
     yield put({ type: 'SEARCH_MOVIES_SUCCESS', payload: response.data.Search });
   } catch (error:any) {
     yield put({ type: 'FETCH_FAILED', payload: error.message });
